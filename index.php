e973ce9f071aca373d29f1f41af65e21d36e84eb
thiago2roshi <ads.thiagosouza@gmail.com>
2016-04-10 17:33:03 -0300

    index.php -> logica construida e teoricamente funcional, realizar testes antes do envio

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
 <?php
 
-try {
-    $conn = new \PDO('mysql:
-                      host=localhost;
-                      dbname=code_pdo',
-                      'root', 'root'
-                    );
-    $query =
+require __DIR__ . '/lib/Conexao.class.php';
+
+$conn = new Conexao();
+
+function exibeAlunos($query)
+{
+    $query;
+    try {
+        $conn->connect();
+        $res = $conn->selectDB($query);
+
+    } catch (\PDOException $e) {
+        echo "Ops, tivemos um problema com o banco de dados" .
+              $e->getCode() . ': ' .
+              $e->getMessage();
+    }
+    // printando table com os resultados da query
+    echo "<table><th>"       .
+             "<td>ID</td>"   .
+             "<td>Nome</td>" .
+             "<td>Nota</td>" .
+         "</th>"
+    foreach ($res as $key) {
+        echo "<tr>" .
+             "<td>" . $res['id']   . "</td>" .
+             "<td>" . $res['nome'] . "</td>" .
+             "<td>" . $res['nota'] . "</td>" .
+             "</tr>"
+    }
+    echo "</table>"
 }
-catch (\PDOException $e){
-    echo "Ops, tivemos um problema com o banco de dados" .
-          $e->getCode() . ': ' .
-          $e->getMessage();
-}
+
+// agora sim o codigo roda
+echo "<h2>exibindo todos os alunos</h2>";
+    exibeAlunos("SELECT * FROM `code_pdo`.`alunos`");
+echo "<h2>exibindo as 3 maiores notas</h2>"
+    exibeAlunos("SELECT * FROM `code_pdo`.`alunos` ORDER BY `nota` DESC LIMIT=3");
